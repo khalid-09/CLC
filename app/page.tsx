6c9fbdb03098c8f01750cb2424eb9bf82e2c6c79
Khalid Ansari <khaliljpc@gmail.com>
2024-05-03 18:54:53 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from '@/components/toggle-theme';
+import { Button } from '@/components/ui/button';
+
+const page = () => {
+  return (
+    <div>
+      <Button>Hello Next</Button>
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default page;
